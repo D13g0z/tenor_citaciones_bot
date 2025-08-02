4586deb8091b2b04f257fd20c38c10bd9e7611a4
Diego Zúñiga <diegocuentas14@gmail.com>
2025-08-02 05:46:20 -0400

    Actualiza formato de ordenanzas según estructura oficial

--- respuestas_medioambiente.py
+++ respuestas_medioambiente.py
@@ -64,15 +64,31 @@ respuestas_medioambiente = {
         "Lo anterior no aplicará en caso de intervenciones de emergencia."
     ),
 
-    'residuospeligrosos': "☣️ *Depositar residuos peligrosos, tóxicos, contaminantes, corrosivos y/o cortantes en recipientes no adecuados.*\n(indicar tipo de residuo)\n**Ordenanza 37 Medio Ambiente – Art. 37°**",
+   "residuospeligrosos": (
+        "☣️ *Ordenanza N°37, Artículo 37*\n"
+        "Depositar residuos peligrosos, tóxicos, contaminantes, corrosivos y/o cortantes en recipientes no adecuados.\n"
+        "(indicar tipo de residuo)"
+    ),
+    "basura": (
+        "🗑️ *Ordenanza N°37, Artículo 39*\n"
+        "Abandonar basura o bienes muebles en vía pública.\n"
+        "(ejemplo: muebles, colchones, cocina, etc.)"
+    ),
+    "lavado": (
+        "🚿 *Ordenanza N°37, Artículo 44*\n"
+        "Realizar lavado de vehículos, bienes, elementos u otros en la vía pública."
+    ),
+    "agua": (
+        "💧 *Ordenanza N°37, Artículo 44*\n"
+        "Escurrir agua (potable, servidas, grises) hacia el bien nacional de uso público."
+    ),
+    "calefaccion": (
+        "🔥 *Ordenanza N°37, Artículo 50*\n"
+        "Mantener encendido chimeneas, braseros o calefactores a leña durante episodios de emergencia o preemergencia ambiental."
+    ),
+    "ruidos": (
+        "🔊 *Ordenanza N°37, Artículo 60*\n"
+        "Generar ruidos molestos (música a alto volumen, uso de maquinarias o parlantes) que afecten la convivencia vecinal."
+    )
 
-    'basura': "🗑️ *Abandonar basura o bienes muebles en vía pública.*\n(ejemplo: muebles, colchones, cocina, etc.)\n**Ordenanza 37 Medio Ambiente – Art. 39°**",
-
-    'lavado': "🚿 *Realizar lavado de vehículos, bienes, elementos u otros en la vía pública.*\n**Ordenanza 37 Medio Ambiente – Art. 44°**",
-
-    'agua': "💧 *Escurrir agua (potable, servidas, grises) hacia el bien nacional de uso público.*\n**Ordenanza 37 Medio Ambiente – Art. 44°**",
-
-    'calefaccion': "🔥 *Mantener encendido chimeneas, braseros o calefactores a leña durante episodios de emergencia o preemergencia ambiental.*\n**Ordenanza 37 Medio Ambiente – Art. 50°**",
-
-    'ruidos': "🔊 *Generar ruidos molestos (música a alto volumen, uso de maquinarias o parlantes) que afecten la convivencia vecinal.*\n**Ordenanza 37 Medio Ambiente – Art. 60°**"
 }
